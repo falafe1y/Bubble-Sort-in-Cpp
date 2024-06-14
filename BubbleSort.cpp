f869ad2a88b2498fdf3c2f0cf1229732280dd83f
falafe1y <falafelyy@ya.ru>
2024-06-14 23:06:55 +0300

    That was mistake, I'm sorry

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-void bubbleSort(vector<int> *arr, int n) {
+void BubbleSort(vector<int> *arr, int n) {
     for(int i=0; i < n-1; i++) {
         for(int j=1; j < n-1-i; j++) {
             if((*arr)[j] > (*arr)[j + 1]) {
